6a2349472bda340857e963a6b70359d08f015d2c
Sharif Elmasry <Sharif.Mohammad02@eng-st.cu.edu.eg>
2024-02-18 02:49:37 +0200

    add functionality BE create employee

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -22,7 +22,19 @@ exports.deleteEmployee = async (req, res, next) => {
 // TODO
 exports.createEmployee = async (req, res, next) => {
   const { id, name } = req.body;
+   // Check if employee with the same ID already exists
+   const existingEmployee = employee.find(emp => emp.id === id);
+   if (existingEmployee) {
+     return res.status(409).json({ success: false, message: 'Employee with the same ID already exists' });
+   }  
+
+    // Check if ID and name are provided
+  if (!id || !name) {
+    return res.status(400).json({ success: false, message: 'Please provide both ID and name' });
+  }
+ 
   employee.push({ id, name });
+
   res.status(201).json({ success: true, message: "Employee created successfully" });
 };
 
